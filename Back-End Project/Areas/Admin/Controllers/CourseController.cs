708bc8dcc7f370528c3262317d5d6578d2b002d1
asmannn18 <asimangj@code.edu.az>
2023-05-30 03:53:44 +0400

    courses CRUD is done

--- Back-End Project/Areas/Admin/Controllers/CourseController.cs
+++ Back-End Project/Areas/Admin/Controllers/CourseController.cs
@@ -1,38 +1,182 @@
-﻿using Back_End_Project.Contexts;
+﻿using Back_End_Project.Areas.Admin.ViewModels;
+using Back_End_Project.Contexts;
 using Back_End_Project.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using Back_End_Project.Areas.Admin.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 
 namespace Back_End_Project.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CourseController : Controller
     {
+        static double amount = 0;
         private readonly AppDbContext _context;
-        
-        public CourseController(AppDbContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+
+        public CourseController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-
+            ViewBag.root = _webHostEnvironment.WebRootPath;
             List<Course> courses = _context.Courses.ToList();
             return View(courses);
         }
         public IActionResult Create()
         {
-          
+
             return View();
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(CourseViewModel courseViewModel)
+        [ActionName("Create")]
+        public async Task<IActionResult> Create(CourseViewModel courseViewModel)
         {
+
+            string path = _webHostEnvironment.ContentRootPath + "\\wwwroot\\img\\course\\" + "eheehe-" + amount + courseViewModel.Image.FileName;
+
+
+            Course course = new()
+            {
+                Name = courseViewModel.Name,
+                Image = "eheehe-" + amount + courseViewModel.Image.FileName,
+                Desc = courseViewModel.Desc,
+                Price = courseViewModel.Price,
+                Assesments = courseViewModel.Assesments,
+                SkillLevel = courseViewModel.SkillLevel,
+                StartTime = courseViewModel.StartTime,
+                StudentCount = courseViewModel.StudentCount,
+                ClassDuration = courseViewModel.ClassDuration,
+                Duration = courseViewModel.Duration,
+                Id = courseViewModel.Id,
+
+
+            }
+            ;
+
+            amount += 123;
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+
+            using (FileStream stream = new(path, FileMode.Create))
+            {
+                await courseViewModel.Image.CopyToAsync(stream);
+            };
+
             return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Course? course = await _context.Courses.FirstOrDefaultAsync(x => x.Id == id);
+            if (course == null)
+                return NotFound();
+
+            return View(course);
+
+
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            Course? course = await _context.Courses.FirstOrDefaultAsync(c => c.Id == id);
+            if (course == null)
+                return NotFound();
+
+            string path = _webHostEnvironment.ContentRootPath + "\\wwwroot\\img\\course\\" + course.Image;
+
+            if (System.IO.File.Exists(path))
+            {
+
+                System.IO.File.Delete(path);
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        public async Task<IActionResult> Update(int id)
+        {
+            Course? course=await _context.Courses.FirstOrDefaultAsync(x => x.Id == id);
+            if (course == null)
+                return NotFound();
+
+            CourseViewModel courseViewModel = new()
+            {
+
+                Name = course.Name,
+                Desc = course.Desc,
+                Price = (int)course.Price,
+                Assesments = course.Assesments,
+                SkillLevel = course.SkillLevel,
+                StartTime = course.StartTime,
+                StudentCount = course.StudentCount,
+                ClassDuration = course.ClassDuration,
+                Duration = course.Duration,
+              
+
+            };
+
+
+            return View(courseViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id,CourseViewModel courseViewModel)
+        {
+
+            Course? course= await _context.Courses.FirstOrDefaultAsync(x => x.Id == id);
+            if (course == null)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View();
+            string path = _webHostEnvironment.ContentRootPath + "\\wwwroot\\img\\course\\" ;
+            string path2 = _webHostEnvironment.ContentRootPath + "\\wwwroot\\img\\course\\" + "eheehe-" + amount;
+
+            if (courseViewModel.Image != null)
+            {
+                if(System.IO.File.Exists(path+course.Image))
+                    System.IO.File.Delete(path+course.Image);
+
+                course.Image = "eheehe-" + amount+courseViewModel.Image.FileName;
+                using(FileStream stream = new(path2+ courseViewModel.Image.FileName, FileMode.Create))
+                {
+                    await courseViewModel.Image.CopyToAsync(stream);
+                }
+            amount += 123;
+            }
+            course.Name = courseViewModel.Name;
+            course.Assesments = courseViewModel.Assesments;
+            course.Desc = courseViewModel.Desc;
+            course.Price = courseViewModel.Price;
+            course.StartTime = courseViewModel.StartTime;
+            course.ClassDuration= courseViewModel.ClassDuration;
+            course.Duration = courseViewModel.Duration;
+            course.SkillLevel = courseViewModel.SkillLevel;
+            course.StudentCount=courseViewModel.StudentCount;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
+
+
+
+
+
--- Back-End Project/Areas/Admin/ViewModels/CourseViewModel.cs
+++ Back-End Project/Areas/Admin/ViewModels/CourseViewModel.cs
@@ -1,17 +1,33 @@
-﻿namespace Back_End_Project.Areas.Admin.ViewModels
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Back_End_Project.Areas.Admin.ViewModels
 {
     public class CourseViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public IFormFile Image { get; set; }
+        
+        public IFormFile? Image { get; set; }
+        [Required]
         public string Desc { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
         public DateTime StartTime { get; set; }
+  
+        [Required]
         public int Duration { get; set; }
+        
+        [Required]
         public int ClassDuration { get; set; }
+        [Required]
         public string SkillLevel { get; set; }
+        [Required]
         public byte StudentCount { get; set; }
+        [Required]
         public string Assesments { get; set; }
     }
 }
